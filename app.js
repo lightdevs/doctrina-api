dffafab05ffdbb0264f7c6b6b663d2e1c79480a9
Vlad <vladyslav.priad@gmail.com>
2020-10-16 22:24:46 +0300

    Apollo works

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-require('dotenv').config()
-const express = require("express")
-const { graphqlHTTP } = require('express-graphql');
-const mongoose = require("mongoose")
-const graphqlSchema = require("./graphql/schema")
-const graphqlResolvers = require("./graphql/resolvers")
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema: graphqlSchema,
-        rootValue: graphqlResolvers,
-        graphiql: true,
-    })
-)
+const typeDefs = gql`
+type Query {
+    hello: String
+}
+`;
 
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose
-    .connect(process.env.DB_URL, options)
-    .then(() => app.listen(5000, console.log("Server is running")))
-    .catch(error => {
-        throw error
-    })
+const resolvers = {
+    Query: {
+        hello: () => "Hello"
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({app});
+
+app.listen({port: 5000}, () => {
+    console.log(`Example app listening ${server.graphqlPath}`)
+  })
