ae88fcb115e6223f9b205ec6ab334023d3474eb4
zlobste <mykola.krainiuk@nure.ua>
2020-10-24 23:33:30 +0300

    Add header and footer.

--- src_frontend/DoctrinaUI/src/app/app.module.ts
+++ src_frontend/DoctrinaUI/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { RouterModule } from '@angular/router';
 import { Interceptor } from './core/interceptor/interceptor';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
