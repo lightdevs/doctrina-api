e16c9d55c811685fdfdd81c1b31bd8f8a50e1dbf
zlobste <mykola.krainiuk@nure.ua>
2020-10-26 23:51:30 +0200

    Add course creation form.

--- src_frontend/DoctrinaUI/src/app/features/courses/courses/courses.component.ts
+++ src_frontend/DoctrinaUI/src/app/features/courses/courses/courses.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {Apollo} from 'apollo-angular';
+import { Apollo } from 'apollo-angular';
 import { CreateCourseComponent } from '../create-course/create-course.component';
 import gql from 'graphql-tag';
-
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -15,8 +15,13 @@ export class CoursesComponent implements OnInit {
 
   courses = [];
   loading = false;
+  animal: string;
+  name: string;
 
-  constructor(private apollo: Apollo) {}
+  constructor(
+    private apollo: Apollo,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.apollo
@@ -41,4 +46,22 @@ export class CoursesComponent implements OnInit {
         }
       );
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CreateCourseComponent, {
+      width: '400px',
+      height: '400px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+}
+
+export interface DialogData {
+  animal: string;
+  name: string;
 }
