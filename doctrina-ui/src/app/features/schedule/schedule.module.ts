0efa512fc173145c1d7ad87bb83f29124ca1f0db
zlobste <nkrayniuk@gmail.com>
2020-12-14 00:03:41 +0200

    Fix bug with module.

--- doctrina-ui/src/app/features/schedule/schedule.module.ts
+++ doctrina-ui/src/app/features/schedule/schedule.module.ts
@@ -1,18 +1,20 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ScheduleComponent} from './schedule/schedule.component';
 import {SharedModule} from '../../shared/shared.module';
 import {MaterialModule} from '../../shared/material.module';
+import {CreateScheduleGroupComponent} from './create-schedule-group/create-schedule-group.component';
 
 
 @NgModule({
   declarations: [
-    ScheduleComponent
+    ScheduleComponent,
+    CreateScheduleGroupComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
-    MaterialModule
+    MaterialModule,
   ]
 })
 export class ScheduleModule {
--- doctrina-ui/src/app/features/schedule/schedule/schedule.component.ts
+++ doctrina-ui/src/app/features/schedule/schedule/schedule.component.ts
@@ -1,15 +1,5 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Apollo, gql} from 'apollo-angular';
-import {IUserInfo} from 'src/app/core/interfaces/user.interface';
-import {AuthenticationService} from '../../authentication/authentication.service';
-import {ScheduleService} from '../schedule.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subject} from 'rxjs';
-import {Message} from '../../../core/extension/messages';
-import {FormGroup, FormBuilder, Validators, FormGroupDirective} from '@angular/forms';
-import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
-import {configureToastr, toastrTitle} from '../../../core/helpers';
-import {ToastrService} from 'ngx-toastr';
-import {Router} from '@angular/router';
 
 
 @Component({
@@ -19,28 +9,12 @@ import {Router} from '@angular/router';
 })
 export class ScheduleComponent implements OnInit, OnDestroy {
 
-  @ViewChild(FormGroupDirective) formDirective: FormGroupDirective;
-
-  currentUser: IUserInfo;
-  message = Message;
-  updateProfileForm: FormGroup;
-
-
   private destroy$ = new Subject<void>();
 
-  constructor(
-    private profileService: ScheduleService,
-    private apollo: Apollo,
-    private formBuilder: FormBuilder,
-    public dialog: MatDialog,
-    private toastr: ToastrService,
-    private authService: AuthenticationService,
-    private router: Router
-  ) {
+  constructor() {
   }
 
   ngOnInit() {
-    configureToastr(this.toastr);
   }
 
   ngOnDestroy() {
