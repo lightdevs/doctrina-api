88008dbfe3180bfe8175b7cc6ea8be70326e10bd
zlobste <nkrayniuk@gmail.com>
2020-12-14 18:02:40 +0200

    Add method to get full courses.

--- doctrina-ui/src/app/features/schedule/schedule.service.ts
+++ doctrina-ui/src/app/features/schedule/schedule.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
+import {Observable} from "rxjs";
 
 
 @Injectable({
@@ -7,5 +8,32 @@ import {Apollo, gql} from 'apollo-angular';
 })
 export class ScheduleService {
   constructor(private apollo: Apollo) {
+  }
+
+
+  getFullCoursesByPerson(userId: string): Observable<any> {
+    return this.apollo.query<any>({
+      query: gql`query fullCoursesByPerson($id: String!) {
+        fullCoursesByPerson(id: $id) {
+          course {
+            _id
+            title
+          }
+          lessons {
+            lesson {
+              _id
+              title
+            }
+            tasks {
+              _id
+              title
+            }
+          }
+        }
+      }`,
+      variables: {
+        id: userId,
+      },
+    });
   }
 }
