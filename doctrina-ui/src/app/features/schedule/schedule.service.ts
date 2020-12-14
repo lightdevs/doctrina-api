5276549d9b710e342b96157470aa2ce79c98a623
zlobste <nkrayniuk@gmail.com>
2020-12-14 21:18:19 +0200

    Add methods to add a new grop ellements.

--- doctrina-ui/src/app/features/schedule/schedule.service.ts
+++ doctrina-ui/src/app/features/schedule/schedule.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
 import {Observable} from "rxjs";
+import {ICreateCourseForm} from "../../core/interfaces/course.interface";
 
 
 @Injectable({
@@ -32,6 +33,77 @@ export class ScheduleService {
       }`,
       variables: {
         id: userId,
+      },
+    });
+  }
+
+  createGroup(title: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+      mutation createGroup($title: String!) {
+        createCourse(title: $title) {
+          _id
+          title
+        }
+      }
+    `,
+      variables: {
+        title
+      },
+    });
+  }
+
+
+  addGroupCourse(idGroup: string, idCourse: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+      mutation addGroupCourse(idGroup: ID!, idCourse: ID!) {
+        addGroupCourse(idGroup: $idGroup, idCourse: $idCourse) {
+          courses {}
+          lessons {}
+          tesks {}
+        }
+      }
+    `,
+      variables: {
+        idGroup,
+        idCourse,
+      },
+    });
+  }
+
+  addGroupLesson(idGroup: string, idLesson: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+      mutation addGroupLesson(idGroup: ID!, idLesson: ID!) {
+        addGroupLesson(idGroup: $idGroup, idLesson: $idLesson) {
+          courses {}
+          lessons {}
+          tesks {}
+        }
+      }
+    `,
+      variables: {
+        idGroup,
+        idLesson,
+      },
+    });
+  }
+
+  addGroupTask(idGroup: string, idTask: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+      mutation addGroupTask(idGroup: ID!, idTask: ID!) {
+        addGroupTask(idGroup: $idGroup, idTask: $idTask) {
+          courses {}
+          lessons {}
+          tesks {}
+        }
+      }
+    `,
+      variables: {
+        idGroup,
+        idTask,
       },
     });
   }
