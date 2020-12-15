e9e42e1ccba349382efcdf3b801af53f2448ad0a
Vlad <2000californiadream2000@gmail.com>
2020-12-15 22:02:02 +0200

    heroku

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 5000,
+    PORT: process.env.PORT || 5000,
     database: "mongodb+srv://doctrina:doctrina@doctrina.zodyy.mongodb.net/doctrina?retryWrites=true&w=majority" ,  //process.env.DB_URI,
     secret: "sfdbfgfbfbdfvd678678987ygjggj" //process.env.SECRET
 }
